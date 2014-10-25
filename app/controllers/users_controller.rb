ef78218e8ddd3870550f2f5fbb947c6f0652efc6
Matt Slotkin <matthew.slotkin@gmail.com>
2014-10-25 19:07:55 -0400

    logins working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+  	  log_in @user
       flash[:success] = "Hurray! You are all signed up!"
   	  redirect_to @user
   	else
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get 'users/new'
 
-  root 'application#hello'
+  root 'sessions#new'
 
 
   get 'signup' => 'users#new'
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   resources :users
 
 end
