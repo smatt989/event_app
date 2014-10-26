965e534fa581fa694e4d927296a0966011b765ab
Matt Slotkin <matthew.slotkin@gmail.com>
2014-10-25 23:00:19 -0400

    added password resets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_reset/new'
+
+  get 'password_reset/edit'
+
   get 'account_activations/edit'
 
   get 'sessions/new'
@@ -17,5 +21,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141025233532) do
+ActiveRecord::Schema.define(version: 20141026025815) do
 
   create_table "users", force: true do |t|
     t.string   "firstName"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20141025233532) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
