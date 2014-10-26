05efd2a3ae0319944a24a9fa4b55f0786a7a80a0
Matt Slotkin <matthew.slotkin@gmail.com>
2014-10-25 20:30:42 -0400

    adding right host url

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "noreply@localhost:3000"
+  default from: "noreply@polar-falls-4416.herokuapp.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
